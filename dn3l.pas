174bd9ed5a1fca2bd9f3461e50877c3f58cc2ede
Ivan Sorokin <unxed@mail.ru>
2025-05-23 15:46:36 +0200

    Enabling UTF8 support for Windows console users

--- dn3l.pas
+++ dn3l.pas
@@ -4,12 +4,20 @@ uses
   {$IFDEF UNIX}
   cthreads,
   {$ENDIF}
+  {$IFDEF WINDOWS}
+  Windows,
+  {$ENDIF}
   dnapp; // Наш основной модуль приложения
 
 // DNApplication объявлена как PTDNApplication в dnapp.pas,
 // поэтому она уже является указателем.
 
 begin
+  {$IFDEF WINDOWS}
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8);  
+  {$ENDIF}
+
   New(DNApplication, Init); // Правильный вызов конструктора для типа object через New
   DNApplication^.Run;      // Обращение к методам через указатель
   Dispose(DNApplication, Done); // Правильный вызов деструктора
